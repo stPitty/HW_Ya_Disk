e3f90d392b018d7831e8c40b9a584c5bbe3f1f56
Petr Stankin <stanoke@mail.ru>
2021-07-02 02:10:53 +0300

    Now it can work with list of files

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 import os
 
 my_token = "AQAAAAAnK65CAADLW49TAXPXR0YRvGe14ErHUgc"
+my_list = ['/Users/rup/Work/BTC/Shit/Contact-57369037-attachment-305508360.png',
+           '/Users/rup/Work/BTC/profilowej-ikony-avatar-mężczyzna-modnisia-stylu-męska-moda-62449823.jpg',
+           '/Users/rup/Work/BTC/Proxy boto.txt']
 
 class Ya_Disk:
 
-    def __init__(self,token,file_path: str):
+    def __init__(self,token,file_path_list):
         self.token = token
-        self.file_path = file_path
-        self.file_name = os.path.basename(self.file_path)
+        self.file_path_list = file_path_list
 
     def get_headers(self):
         return {
@@ -22,23 +24,24 @@ class Ya_Disk:
         response = requests.get(url, headers=headers)
         return response.json()
 
-    def _get_upload_link(self):
+    def _get_upload_link(self,file_name):
         url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
-        params = {"path":self.file_name, "overwrite":'true'}
+        params = {"path":file_name, "overwrite":'true'}
         response = requests.get(url, headers=headers, params=params)
         return response.json()
 
     def upload_files(self):
-        href = self._get_upload_link().get('href','')
-        response = requests.put(href, data=open(self.file_path, 'rb'))
-        if response.status_code == 201:
-            print(f'Файл {self.file_name} успешно загружен на Я.Диск')
-        else:
-            print('Что-то пошло не так')
-            return response.status_code
+        added_files = []
+        for file in self.file_path_list:
+            yandex_file_path = os.path.basename(file)
+            href = self._get_upload_link(yandex_file_path).get('href','')
+            response = requests.put(href, data=open(file, 'rb'))
+            if response.status_code == 201:
+                added_files += [yandex_file_path]
+        return print(f'Файлы {", ".join(added_files)} успешно загружены на ЯДиск')
 
 
-yandex = Ya_Disk(my_token,'My_Files/example.py')
+yandex = Ya_Disk(my_token,my_list)
 
 yandex.upload_files()
